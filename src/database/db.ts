92eb4faa538e72bece208d99dd7dd59776b8a624
darzizalol <darzizalol@gmail.com>
2025-05-26 12:03:12 +0800

    zhenghao: Blending in SQLite and a bunch of test files for SQLite

---
+++ src/database/db.ts
@@ -0,0 +1,37 @@
+import Database from 'better-sqlite3';
+import path from 'path';
+import { app } from 'electron';
+import fs from 'fs';
+
+// This is where we store our database file
+const userDataPath = app ? app.getPath('userData') : path.join(__dirname, 'test-db');
+
+// Used for testing purposes
+// This code runs only if userDataPath does not exist and creates a parent directory
+if (!fs.existsSync(userDataPath)) {
+    fs.mkdirSync(userDataPath, { recursive: true });
+}
+
+// Creates a full path to database file, and naming it as 'inkflow.db'
+const dbPath = path.join(userDataPath, 'inkflow.db');
+
+// Create a new database connection
+const db = new Database(dbPath);
+
+// Enable foreign key support
+// Note: This ensures that relationships between tables are valid
+// E.g. If a not references a category, that category must exist
+db.pragma('foreign_keys = ON');
+
+// Create our notes table if it does not exist
+db.exec(`
+    CREATE TABLE IF NOT EXISTS notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        title TEXT NOT NULL,
+        content TEXT,
+        created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+        updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
+    );
+`);
+
+export default db;
