79f63718b7a9869faa5e578af6af113d881d300e
Abhijay <abhijay3852@gmail.com>
2025-05-26 15:48:51 +0800

    Abhijay: Test Boxes and Split Containers in app

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,16 @@
-
+import { Box } from "@mui/material";
+import Split from "react-split";
 
 function App() {
   return (
     <>
-      
+      <Box sx = {{ height: "100vh", width: "100vw", overflow: "hidden"}}>
+        <Split sizes= {[30, 70]}
+        minSize= {200}
+        direction= "horizontal"
+        style={{ display: "flex", height: "100%"}}
+        ></Split>
+      </Box>
     </>
   )
 }
