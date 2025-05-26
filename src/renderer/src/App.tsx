23d2c46c1aa1464932c8cd845f6c43da99a0cd2e
Abhijay <abhijay3852@gmail.com>
2025-05-26 17:41:16 +0800

    Abhijay: Added Basic UI Design (File System, Content Window)

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,15 +1,42 @@
-import { Box } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import Split from "react-split";
 
 function App() {
   return (
     <>
-      <Box sx = {{ height: "100vh", width: "100vw", overflow: "hidden"}}>
-        <Split sizes= {[30, 70]}
-        minSize= {200}
-        direction= "horizontal"
-        style={{ display: "flex", height: "100%"}}
-        ></Split>
+      <Box sx={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
+        <Split sizes={[14, 86]}
+          minSize={[14, 86]}
+          direction="horizontal"
+          style={{
+            display: "flex",
+            height: "100%"
+          }}
+        >
+          <Stack sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            bgcolor: "grey",
+            m: 1, mr: 0.01,
+            borderRadius: 2
+          }}
+          >
+            File System
+          </Stack>
+
+          <Box sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            bgcolor: "grey",
+            m: 1, ml: 0.01,
+            borderRadius: 2
+          }}
+          >
+            Content Window
+          </Box>
+        </Split>
       </Box>
     </>
   )
