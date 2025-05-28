767d2ab431391551d272a30499572b4a8559fc79
Abhijay <abhijay3852@gmail.com>
2025-05-28 15:23:10 +0800

    Abhijay: (QoL) Added Cosmetic Changes For Background

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,8 @@ function createWindow(): void {
     width: 900,
     height: 670,
     show: false,
+    vibrancy: "under-window",
+    visualEffectState: "active", 
     autoHideMenuBar: true,
     ...(process.platform === 'linux' ? { icon } : {}),
     webPreferences: {
