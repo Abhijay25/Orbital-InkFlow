1d5d9abfaa17b892aeaa0479788ab8ecfe88d4a0
darzizalol <darzizalol@gmail.com>
2025-05-30 13:29:21 +0800

    zhenghao (QoL): binding 'a file click to fileID state'

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,11 +2,27 @@ import { Box } from "@mui/material";
 import Split from "react-split";
 import FileSystem from "./components/FileSystem/FileSystem";
 import Content from "./components/Content";
+import { useState, createContext } from "react";
+
+export type FileIDContextType = {
+  fileID: string;
+  setFileID: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export const FileIDContext = createContext<FileIDContextType>({
+  fileID: "",
+  setFileID: () => {},
+});
 
 function App() {
+
+  const [fileID, setFileID] = useState<string>("")
+
+  console.log(fileID);
+
   return (
     <>
-      <Box sx={{ height: "100vh", width: "100vw", overflow: "hidden" }}>   
+      <Box sx={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
         <Split sizes={[14, 86]}
           minSize={[14, 86]}
           direction="horizontal"
@@ -15,12 +31,12 @@ function App() {
             height: "100%"
           }} // Container Box and Split styling 
         >
-
-          <FileSystem /> 
-          <Content />
-          
-        </Split> 
-      </Box> 
+          <FileIDContext.Provider value={{ fileID, setFileID }}>
+            <FileSystem />
+            <Content />
+          </FileIDContext.Provider>
+        </Split>
+      </Box>
     </>
   )
 }
