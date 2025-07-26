15e1627fca56b07f90c5651f4c0d13d182dc6d05
Abhijay <abhijay3852@gmail.com>
2025-07-27 00:08:21 +0800

    Abhijay: (Chore) Added Clock and Welcome to HomePage

--- src/renderer/src/components/HomePage.tsx
+++ src/renderer/src/components/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Box } from "@mui/material";
-
+import Clock from "react-live-clock";
 import "../styles/Home.css";
 
 function HomePage(): React.ReactElement | null {
@@ -10,13 +10,17 @@ function HomePage(): React.ReactElement | null {
         position: "relative",
         alignItems: "center",
         justifyContent: "center",
-        m: 1,
-        ml: 0.01,
-        borderRadius: 1,
       }}
       className="homepage"
     >
-        <h1>Welcome Back!</h1>
+        <h1 className="welcome">Welcome Back!</h1>
+        <Clock 
+          format={'HH:mm:ss'}
+          ticking={true}
+          className="clock"
+          timezone="Asia/Singapore"
+        />
+        
     </Box>
   )
 }
